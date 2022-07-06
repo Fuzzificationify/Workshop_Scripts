67ee84cff009e7a748a720f66b5b6addd0bb7c83
Fuzzificationify <101608686+Fuzzificationify@users.noreply.github.com>
2022-07-06 11:51:32 +0100

    Update Bake_to_Layer.py

--- Bake_to_Layer.py
+++ Bake_to_Layer.py
@@ -3,9 +3,13 @@ import maya.cmds as mc
 sel = mc.ls(sl=1)
 animLayer_name = sel[0] + "_base"
 
+minTime = mc.playbackOptions(q=1, minTime=1)
+maxTime = mc.playbackOptions(q=1, maxTime=1)
+time_range = minTime, maxTime
+
 mc.animLayer(animLayer_name, override=1, addSelectedObjects=1, extractAnimation="BaseAnimation")
 
-mc.bakeResults(sel, time=(10, 100), bakeOnOverrideLayer=True, preserveOutsideKeys=True)
+mc.bakeResults(sel, time=time_range, bakeOnOverrideLayer=True, preserveOutsideKeys=True)
 
 if mc.animLayer(sel, q=1, affectedLayers=1) and mc.animLayer('BakeResults', q=1, exists=1):
     mc.rename('BakeResults', sel[0] + "_bk_lyr")
