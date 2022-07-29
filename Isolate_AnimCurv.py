9ad9d049f2eb09c5a66740ebf49e5b2accd14c67
Fuzzificationify <101608686+Fuzzificationify@users.noreply.github.com>
2022-07-29 14:17:06 +0100

    Create Isolate_AnimCurv.py

---
+++ Isolate_AnimCurv.py
@@ -0,0 +1,11 @@
+import maya.cmds as mc
+import maya.mel as mel
+
+
+curveSelection = mc.keyframe(query=True, selected=True, name=True)
+
+if curveSelection:
+    mel.eval("isolateAnimCurve true graphEditor1FromOutliner graphEditor1GraphEd")
+
+else:
+    mel.eval("isolateAnimCurve false graphEditor1FromOutliner graphEditor1GraphEd")
