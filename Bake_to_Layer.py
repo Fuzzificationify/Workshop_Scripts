0a732372fa58ea7c5a0f750f841928f532fe2803
Fuzzificationify <101608686+Fuzzificationify@users.noreply.github.com>
2022-08-03 17:12:40 +0100

    Update Bake_to_Layer.py

--- Bake_to_Layer.py
+++ Bake_to_Layer.py
@@ -1,3 +1,6 @@
+# Sometimes when baking a certain frame range Maya will delete the existing out-of-range keys.
+# This script extracts the existing animation first to protect it.
+
 import maya.cmds as mc
 
 sel = mc.ls(sl=1)
@@ -7,11 +10,17 @@ minTime = mc.playbackOptions(q=1, minTime=1)
 maxTime = mc.playbackOptions(q=1, maxTime=1)
 time_range = minTime, maxTime
 
-mc.animLayer(animLayer_name, override=1, addSelectedObjects=1, extractAnimation="BaseAnimation")
+# Extract base animation to new layer
+extract_lyr = mc.animLayer(animLayer_name, override=1, addSelectedObjects=1, extractAnimation="BaseAnimation")
 
 mc.bakeResults(sel, time=time_range, bakeOnOverrideLayer=True, preserveOutsideKeys=True)
 
 if mc.animLayer(sel, q=1, affectedLayers=1) and mc.animLayer('BakeResults', q=1, exists=1):
     mc.rename('BakeResults', sel[0] + "_bk_lyr")
 
+# Copy anim back to Base layer and delete
+mc.animLayer('BaseAnimation', e=1, copyAnimation=extract_lyr)
+mc.delete(extract_lyr)    
+    
+    
 mc.delete(sel, constraints=True)
