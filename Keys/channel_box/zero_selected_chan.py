7b03477d480c7a86a3ca9d0d48a203e75665c6cf
Fuzzificationify <101608686+Fuzzificationify@users.noreply.github.com>
2023-05-30 21:46:23 +0000

    Update zero_selected_chan.py

--- Keys/channel_box/zero_selected_chan.py
+++ Keys/channel_box/zero_selected_chan.py
@@ -1,13 +1,12 @@
 # Zero selected channels
 
 import maya.cmds as mc
+import maya.mel as mel
 
 sel = mc.ls(sl=1)
-channels = mc.channelBox('mainChannelBox', q=1, sma=1)
+channels = mel.eval('selectedChannelBoxAttributes;')
 current_time = mc.currentTime(q=1)
 
 for obj in sel:
     for chan in channels:
         mc.setAttr(obj + "." + chan, 0)
-
-
