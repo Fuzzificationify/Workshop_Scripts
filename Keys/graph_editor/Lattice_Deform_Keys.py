cbf2899d6e7338372131a50d4829553320d7d7e4
Fuzzificationify <101608686+Fuzzificationify@users.noreply.github.com>
2023-06-01 00:06:09 +0000

    Update Lattice_Deform_Keys.py

--- Keys/graph_editor/Lattice_Deform_Keys.py
+++ Keys/graph_editor/Lattice_Deform_Keys.py
@@ -1,5 +1,23 @@
-import maya.cmds as cmds
-import maya.mel as mel
-oPanel = cmds.getPanel(wf = True)
-if 'graphEditor' in oPanel:
-    maya.mel.eval('setToolTo latticeDeformKeySuperContext;')
+# 3 x 3 Lattice
+
+if mc.contextInfo("latticeKeyDeform1", exists=1):
+
+	mc.latticeDeformKeyCtx("latticeKeyDeform1", e=1, latticeColumns=3, latticeRows=3)
+	mc.setToolTo("latticeKeyDeform1")
+
+else:
+	mc.latticeDeformKeyCtx("latticeKeyDeform1", latticeColumns=3, latticeRows=3)
+	mc.setToolTo("latticeKeyDeform1")
+    
+    
+    
+# 5 x 5 Lattice
+
+if mc.contextInfo("latticeKeyDeform1", exists=1):
+
+	mc.latticeDeformKeyCtx("latticeKeyDeform1", e=1, latticeColumns=5, latticeRows=5)
+	mc.setToolTo("latticeKeyDeform1")
+
+else:
+	mc.latticeDeformKeyCtx("latticeKeyDeform1", latticeColumns=5, latticeRows=5)
+	mc.setToolTo("latticeKeyDeform1")
